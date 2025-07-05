59a1f40ee1090856c26d216e527df34c8d1c75b0
Mirco Ramundo <mramundo@outlook.it>
2025-07-05 20:13:22 +0200

    Fixes post deploy

--- world-miniapp/next.config.ts
+++ world-miniapp/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["static.usernames.app-backend.toolsforhumanity.com"],
   },
-  allowedDevOrigins: ["*", "*.ngrok-free.app"],
+  allowedDevOrigins: ["*", "*.ngrok-free.app", "*.netlify.app"],
   reactStrictMode: false,
 };
 
